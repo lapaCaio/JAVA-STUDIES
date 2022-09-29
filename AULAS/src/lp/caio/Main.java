19b06e90b97b39ecce1ee63552b1d9a16896c82d
ufes <lapacaio2001@gmail.com>
2022-09-29 17:07:47 -0300

    CREATE "AULA" FOLDER
    create "AULA" folder to organize JAVA codes

---
+++ AULAS/src/lp/caio/Main.java
@@ -0,0 +1,14 @@
+package lp.caio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String a = "Caio";  //variável de pilha
+        String b = new String("caio");
+
+        System.out.println(a.equalsIgnoreCase(b));
+
+    }
+}
