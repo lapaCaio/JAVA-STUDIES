f4e0693e20cfd0d491104d5e5fa0b28494ad56a3
lapaCaio <lapacaio2001@gmail.com>
2022-10-25 16:51:36 -0300

    UPD - Aula03

--- CURSO/AULA03/src/aula03/Main.java
+++ CURSO/AULA03/src/aula03/Main.java
@@ -3,6 +3,14 @@ package aula03;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Caneta c1 = new Caneta();
+
+        c1.modelo = "BIC Cristal";
+        c1.cor = "Azul";
+        //c1.ponta = 0.5f;
+        c1.carga = 80;
+        c1.tampada = true;
+
+        c1.status();
     }
 }
